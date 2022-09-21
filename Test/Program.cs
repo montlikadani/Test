e62b2636b13ee4e381c668d9cdd6770528e11d94
montlikadani <montlikada@gmail.com>
2022-09-21 13:03:57 +0200

    3rd commit

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,12 +6,20 @@ namespace Test {
         static void Main(string[] args) {
             Console.WriteLine("Adjon meg két egész számot:");
 
-            while (!int.TryParse(Console.ReadLine(), out int egesz)) {
+            int egesz, egesz2;
+
+            while (!int.TryParse(Console.ReadLine(), out egesz)) {
                 Console.WriteLine("Adjon meg egy egész számot:");
             }
 
-            while (!int.TryParse(Console.ReadLine(), out int egesz2)) {
+            while (!int.TryParse(Console.ReadLine(), out egesz2)) {
                 Console.WriteLine("Adjon meg egy egész számot:");
+            }
+
+            if (egesz > egesz2) {
+                Console.WriteLine("A nagyobb szám: " + egesz);
+            } else if (egesz2 > egesz) {
+                Console.WriteLine("A nagyobb szám: " + egesz2);
             }
 
             Console.ReadKey();
